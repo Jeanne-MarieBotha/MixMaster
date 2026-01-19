b0dcf36ff8e2d5f6cf22c401688e55e0aa65caaa
Jeanne-MarieBotha <jeannemariebuckle@gmail.com>
2026-01-19 16:13:18 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  About,
+  Landing,
+  HomeLayout,
+  Newsletter,
+  Error,
+  Cocktail,
+  SinglePageError,
+} from "./pages";
+import { loader as landingLoader } from "./pages/Landing";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error/>,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <SinglePageError/>,
+        loader: landingLoader,
+      },
+      {
+        path: "cocktail",
+        element: <Cocktail />,
+      },
+      {
+        path: "newsletter",
+        element: <Newsletter />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
