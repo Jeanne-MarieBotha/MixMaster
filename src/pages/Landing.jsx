c9117490032fc7e9e7e94a715b7fe4b58b90611f
Jeanne-MarieBotha <jeannemariebuckle@gmail.com>
2026-01-20 10:09:42 +0200

    second commit

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useLoaderData } from "react-router-dom";
+import CocktailList from "../components/CocktailList";
+
 
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
@@ -7,13 +9,16 @@ const cocktailSearchUrl =
 export const loader = async () => {
   const searchTerm = "a";
   const response = await axios.get(`${cocktailSearchUrl} ${searchTerm}`);
-  // return { drinks: response.data.drinks, searchTerm };
+  return { drinks: response.data.drinks, searchTerm };
 };
 
 const Landing = () => {
   const {drinks, searchTerm} = useLoaderData();
-  console.log(drinks);
+  
 
-  return <h1>Landing</h1>;
+  return <CocktailList drinks={drinks}/>
+ 
+  
+
 };
 export default Landing;
